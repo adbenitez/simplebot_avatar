a7fd8c3051bbf000e50789b4f111e6480a8b3113
adbenitez <asieldbenitez@gmail.com>
2021-03-24 17:55:03 -0400

    first commit

---
+++ README.rst
@@ -0,0 +1,32 @@
+Avatar
+======
+
+.. image:: https://img.shields.io/github/v/release/simplebot-org/simplebot_avatar
+   :target: https://pypi.org/project/simplebot_avatar
+
+.. image:: https://img.shields.io/pypi/pyversions/simplebot_avatar.svg
+   :target: https://pypi.org/project/simplebot_avatar
+
+.. image:: https://pepy.tech/badge/simplebot_avatar
+   :target: https://pepy.tech/project/simplebot_avatar
+
+.. image:: https://img.shields.io/github/license/simplebot-org/simplebot_avatar
+   :target: https://github.com/simplebot-org/simplebot_avatar/blob/master/LICENSE
+
+.. image:: https://img.shields.io/badge/code%20style-black-000000.svg
+   :target: https://github.com/psf/black
+
+A `SimpleBot`_ plugin that allows users to generate avatars using
+`cat-generator`_ and `bird-generator`_.
+
+Install
+-------
+
+To install run::
+
+  pip install simplebot-avatar
+
+
+.. _SimpleBot: https://github.com/simplebot-org/simplebot
+.. _cat-generator: https://www.peppercarrot.com/extras/html/2016_cat-generator
+.. _bird-generator: https://www.peppercarrot.com/extras/html/2019_bird-generator
---
+++ simplebot_avatar.py
@@ -0,0 +1,67 @@
+import io
+from urllib.parse import quote_plus
+
+import bs4
+import requests
+import simplebot
+from simplebot.bot import Replies
+
+__version__ = "1.0.0"
+HEADERS = {
+    "user-agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:60.0)"
+    " Gecko/20100101 Firefox/60.0"
+}
+
+
+@simplebot.command
+def avatar_cat(payload: str, replies: Replies) -> None:
+    """Generate a cat avatar based on the given text.
+
+    If no text is given a random avatar is generated.
+    """
+    replies.add(**_get_reply(payload, "2016_cat-generator"))
+
+
+@simplebot.command
+def avatar_bird(payload: str, replies: Replies) -> None:
+    """Generate a bird avatar based on the given text.
+
+    If no text is given a random avatar is generated.
+    """
+    replies.add(**_get_reply(payload, "2019_bird-generator"))
+
+
+def _get_reply(text: str, generator: str) -> dict:
+    url = "https://www.peppercarrot.com/extras/html/{}/".format(generator)
+    if not text:
+        with requests.get(url, headers=HEADERS) as resp:
+            resp.raise_for_status()
+            soup = bs4.BeautifulSoup(resp.text, "html.parser")
+        text = soup.find("img", class_="avatar")["src"].rsplit("=", maxsplit=1)[-1]
+
+    url += "avatar.php?seed=" + quote_plus(text)
+    with requests.get(url, headers=HEADERS) as resp:
+        resp.raise_for_status()
+        return dict(
+            text=text, filename="avatar.png", bytefile=io.BytesIO(resp.content)
+        )
+
+
+class TestSendFile:
+    def test_avatar_cat(self, mocker):
+        msg = mocker.get_one_reply("/avatar_cat")
+        assert msg.filename
+        # assert msg.is_image()
+
+        msg = mocker.get_one_reply("/avatar_cat test avatar")
+        assert msg.filename
+        # assert msg.is_image()
+
+    def test_avatar_bird(self, mocker):
+        msg = mocker.get_one_reply("/avatar_bird")
+        assert msg.filename
+        # assert msg.is_image()
+
+        msg = mocker.get_one_reply("/avatar_bird test avatar")
+        assert msg.filename
+        # assert msg.is_image()
